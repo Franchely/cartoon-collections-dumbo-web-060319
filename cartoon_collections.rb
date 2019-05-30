3d5da93cfb7f4fe1e95d0df0ae9d501628e9e445
github username <github email address>
2019-05-30 12:05:48 -0400

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,9 +1,12 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(array)
+  the_line = []
+  array.each.with_index(1) do |name, i|
+     the_line << " #{i}. #{name}"
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(array)
+  array.collect do |word|
+    "#{word.titleize}!"
 end
 
 def long_planeteer_calls# code an argument here
